3d7faca91145867a032cec637fa933624f538cad
Lemuel Colmenares <leelcoma@gmail.com>
2023-03-09 13:04:03 -0600

    agregar archivo

---
+++ variables.js
@@ -0,0 +1,33 @@
+var variable;
+let variable;
+
+const constante = "Hola soy una constante";
+ 
+var a =1; // en VAR puede variar el valor
+
+a = 4;
+console.log(a);
+
+console.log(constante);
+
+let b = 3;
+console.log(b);
+
+b = 5;
+console.log(b); 
+
+
+var variable ="Hola soy una variable VAR"; 
+for (var i=0; i<3; i++) {
+    var variable "soy la segunda VAR";
+}
+console.log(variable)
+
+let variable ="Hola soy una variable LET"; 
+for (var i=0; i<3; i++){
+    let variableLet "soy la segunda LET";
+}
+console.log(variableLet);
+//separacion
+typeof//sirve para que nos diga que tipo de valor es el dato que estamos usando
+console.log(typeof variable)
