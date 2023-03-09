7bdbf3888d359f8e3287830409dcf2b4e5892494
Lemuel Colmenares <leelcoma@gmail.com>
2023-03-09 12:37:17 -0600

    actual

---
+++ vid2.1.js
@@ -0,0 +1,25 @@
+//Tipos primitivos
+
+//number
+1
+
+//strings
+"holaMundo";
+'holaMundo';
+
+//boolean
+true;
+false;
+
+//null y undefind
+null;
+undefined;
+ //null, undefined, false, 0
+console.log(null, undefined, )
+
+ if(true){
+    console.log("cumple")
+}
+    else {
+        console.log("no cumple")
+    }
